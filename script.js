075548745ea797aee5afe810966aeb2e6212314a
Yasir Raza <yasirrazaattari2002@gmail.com>
2024-08-26 11:55:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// You can add JavaScript functionality here if needed
+// Example: Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
